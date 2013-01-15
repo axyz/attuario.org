5094fd82927b377c29124d72534b3172672f2f30
MCM <MCM@server.local>
2013-01-15 15:51:13 +0100

    jade mixins

--- contact.php
+++ contact.php
@@ -19,7 +19,8 @@
 
 	<link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=Kelly+Slab' rel='stylesheet' type='text/css'><?php include("php/meta.php"); meta("Photography"); ?>  <nav class="top-bar fixed">
+	<link href='http://fonts.googleapis.com/css?family=Kelly+Slab' rel='stylesheet' type='text/css'>
+<title>Contacts</title><nav class="top-bar fixed">
     <ul>
       <li class="name"><h1><a href="<?php echo $basedir;?>">Attuario.org</a></h1></li>
       <li class="toggle-topbar"><a href="#"></a></li>
