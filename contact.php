ba9100694487defa6a9f2ec74b23d75420265f58
MCM <MCM@server.local>
2013-01-16 18:42:14 +0100

    -

--- contact.php
+++ contact.php
@@ -27,7 +27,8 @@
     </ul>
     <section>
       <ul class="right">
-        <li><a href="#"><i class="icon-info-sign"></i>About</a></li>
+        <li><a href="#"><i class="icon-info-sign"></i>Chi Siamo</a></li>
+        <li><a href="#"><i class="icon-info-sign"></i>Servizi</a></li>
         <li><a href="<?php echo $basedir;?>contact.php">Contact</a></li>
       </ul>
     </section>
